f4835f4c13fbe0dd0f037634c6bf0b9e51d4b13a
Tyraffe <nathanb1357@gmail.com>
2023-02-07 09:15:18 -0800

    Added Map and Tile classes

---
+++ src/main/java/org/example/Map.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Map {
+
+  public static int MAPSIZE = 20;
+
+  public Tile[][] tileMap = new Tile[MAPSIZE][MAPSIZE];
+  public Sprite[][] spriteMap = new Sprite[MAPSIZE][MAPSIZE];
+}
---
+++ src/main/java/org/example/Tile.java
@@ -0,0 +1,20 @@
+package org.example;
+
+/**
+ * Abstract class for Tile.
+ *
+ * @author Nathan Bartyuk
+ * @version 2023-02-07
+ */
+abstract class Tile {
+  private int x;
+  private int y;
+
+  public int getX() {
+    return x;
+  }
+
+  public int getY() {
+    return y;
+  }
+}
