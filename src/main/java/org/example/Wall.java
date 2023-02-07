4dc8314e38183730fc3cecaf9fbf01805309cb4f
Simrat Kaur <simratkaur2@my.bcit.ca>
2023-02-06 16:30:42 -0800

    Added javadocs for the wall class.

--- src/main/java/org/example/Wall.java
+++ src/main/java/org/example/Wall.java
@@ -1,5 +1,13 @@
 package org.example;
 
+/**
+ * defines the non-movable blocks/boundary for the game that
+ * can be assumed to be walls.
+ *
+ * @author Simrat Kaur
+ * @author ....
+ * @version 1.0, February 6, 2023
+ */
 public class Wall {
 
 }
