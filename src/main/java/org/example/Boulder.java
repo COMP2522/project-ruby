4ab03bfe138ec6850223c233c143ac72dd1810ff
Amrit Jhatu <a_jhatu@hotmail.com>
2023-02-06 16:20:01 -0800

    Added Javadoc and basic comments

--- src/main/java/org/example/Boulder.java
+++ src/main/java/org/example/Boulder.java
@@ -1,5 +1,13 @@
 package org.example;
-
+/**
+ * Defines the Boulder class and the methods that will be implemented for its function.
+ *
+ * @author Amrit Singh
+ * @author etc etc ...
+ * @version 1.0, February 06, 2023
+ */
 public class Boulder {
+
+  // Insert methods here.
 
 }
