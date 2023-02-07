723af4ea9cf0774b709ddd725e7bec8a8afef95d
Amrit Jhatu <a_jhatu@hotmail.com>
2023-02-06 16:34:51 -0800

    Added Javadoc and abstract class

---
+++ src/main/java/org/example/Sprite.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Abstract class for Sprite.
+ *
+ * @author Amrit Singh
+ * @author etc etc ...
+ * @version 1.0, February 06, 2023
+ */
+abstract class Sprite {
+
+
+}
