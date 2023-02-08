a249e6a581edbb137597e6eaf76b41c6fd08e6d1
Tyraffe <nathanb1357@gmail.com>
2023-02-07 16:39:28 -0800

    Added Server and SaveState, also, extending the Tile and Sprite class

---
+++ src/main/java/org/example/Server.java
@@ -0,0 +1,21 @@
+package org.example;
+
+/**
+ * Defines the server, which manages game state.
+ *
+ * @author Nathan Bartyuk
+ * @version 2023-02-07
+ */
+public class Server {
+  private SaveState currentSave;
+
+  public void newGame() {
+    Map map = new Map();
+    Player player = new Player();
+  }
+
+  public void loadGame() {
+    Map map = currentSave.translateMap();
+    Player player = new Player(currentSave.x(), currentSave.y(), currentSave.rubies());
+  }
+}
