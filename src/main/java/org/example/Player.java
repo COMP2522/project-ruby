30f22d329c1b693360e71d4a5b12611a189b4d96
Abhishek Chouhan <abhishekchouhannk@gmail.com>
2023-03-13 19:16:34 -0700

    made the player class displayable and movable

--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -1,9 +1,13 @@
 package org.example;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.lang.management.GarbageCollectorMXBean;
 
 /**
@@ -34,6 +38,7 @@ public class Player implements KeyListener {
     if (code == KeyEvent.VK_D) {
       rightPressed = true;
     }
+    updateDirection(e);
   }
 
   @Override
@@ -51,7 +56,6 @@ public class Player implements KeyListener {
     if (code == KeyEvent.VK_D) {
       rightPressed = false;
     }
-    updateDirection(e);
 //    System.out.println(currentDirection);
   }
 
@@ -67,15 +71,18 @@ public class Player implements KeyListener {
   private directions currentDirection;
   private status currentStatus;
 
+  public BufferedImage down, up, right, left;
+
 
   // Sets up fresh player upon starting a new game.
   public Player() {
     this.currentLives = LIVES;
     this.currentRubies = 0;
-    this.currentDirection = directions.RIGHT;
+    this.currentDirection = directions.DOWN;
     this.currentStatus = status.ALIVE;
-    this.x = 100;
-    this.y = 100;
+    this.x = 150;
+    this.y = 50;
+    getPlayerImage();
   }
 
   // Sets up the player from an existing save.
@@ -98,6 +105,19 @@ public class Player implements KeyListener {
       currentDirection = directions.UP;
     } else if (code == KeyEvent.VK_S) {
       currentDirection = directions.DOWN;
+    }
+  }
+
+  public void getPlayerImage() {
+    try{
+      down = ImageIO.read(new FileInputStream("res/player/Player_DRight.png"));
+      up = ImageIO.read(new FileInputStream("res/player/Player_DRight.png"));
+      left = ImageIO.read(new FileInputStream("res/player/Player_DLeft.png"));
+      right = ImageIO.read(new FileInputStream("res/player/Player_DRight.png"));
+//      downRight = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/Player_DRight.png"));
+    } catch(IOException e) {
+      System.out.println("Image can't be read");
+      e.printStackTrace();
     }
   }
 
@@ -132,8 +152,29 @@ public class Player implements KeyListener {
 
   // to draw the player sprite
   public void draw(Graphics2D g2, GamePanel p) {
-    g2.setColor(Color.white);
-    g2.fillRect(x, y, p.tileSize, p.tileSize);
+    BufferedImage image = null;
+
+    switch(currentDirection) {
+      case UP:
+        image = up;
+        break;
+      case DOWN:
+        image = down;
+        break;
+      case LEFT:
+        image = left;
+        break;
+      case RIGHT:
+        image = right;
+        break;
+      default:
+        break;
+    }
+    g2.drawImage(image, x, y, p.tileSize, p.tileSize, null);
+
+    // below is the stupid code for rectangle I used for testing
+//    g2.setColor(Color.white);
+//    g2.fillRect(x, y, p.tileSize, p.tileSize);
   }
 
   public static void main(String[] args) {
