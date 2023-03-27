888eb6143c47172cea3d04f257b1aaffc7418d4a
Abhishek Chouhan <abhishekchouhannk@gmail.com>
2023-03-26 18:43:22 -0700

    added code to handle keyevent in KeyHandler

---
+++ src/main/java/org/sourceCode/KeyHandler.java
@@ -0,0 +1,52 @@
+package org.sourceCode;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+
+  public static KeyEvent e;
+
+  public boolean upPressed, downPressed, leftPressed, rightPressed; // keys pressed
+
+  @Override
+  public void keyTyped(KeyEvent e) {}  // not using this one garbage
+
+  @Override
+  public void keyPressed(KeyEvent e) {
+    this.e = e;
+    int code = e.getKeyCode();
+    if (code == KeyEvent.VK_W) {
+      upPressed = true;
+    }
+    if (code == KeyEvent.VK_S) {
+      downPressed = true;
+    }
+    if (code == KeyEvent.VK_A) {
+      leftPressed = true;
+    }
+    if (code == KeyEvent.VK_D) {
+      rightPressed = true;
+    }
+  }
+
+  @Override
+  public void keyReleased(KeyEvent e) {
+    this.e = e;
+    int code = e.getKeyCode();
+    if (code == KeyEvent.VK_W) {
+      upPressed = false;
+    }
+    if (code == KeyEvent.VK_S) {
+      downPressed = false;
+    }
+    if (code == KeyEvent.VK_A) {
+      leftPressed = false;
+    }
+    if (code == KeyEvent.VK_D) {
+      rightPressed = false;
+    }
+//    System.out.println(currentDirection);
+  }
+
+}
