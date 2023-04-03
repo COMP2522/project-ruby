b6264db935151460e7fb44f87a8de9a34dda3dfc
Greg Song <gregjsong@gmail.com>
2023-04-03 09:18:04 -0700

    Changed Client and Server to use savestate.getinstance().

--- src/main/java/org/project/Server.java
+++ src/main/java/org/project/Server.java
@@ -86,7 +86,7 @@ public class Server {
       }
 
 //      test
-      SaveState savestate = new SaveState();
+      SaveState savestate = SaveState.getInstance();
       
       // close resources TODO: close ois this without crashing
       // crashing when closing ois here with postreqhandler
@@ -104,13 +104,7 @@ public class Server {
     // close the ServerSocket object
     server.close();
   }
-  
-  public void newGame() {
-  }
-  
-  public void loadGame() {
-  }
-  
+
   public static void main(String args[]) throws IOException, ClassNotFoundException, InterruptedException {
     Server server = new Server();
     server.start();
