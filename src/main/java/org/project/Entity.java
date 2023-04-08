0d3ce42814e1515d4b15cdc385f150f618bf9ea4
Abhishek Chouhan <abhishekchouhannk@gmail.com>
2023-04-08 13:18:26 -0700

    refactored monster/entity class as they had some issues

--- src/main/java/org/project/Entity.java
+++ src/main/java/org/project/Entity.java
@@ -117,74 +117,7 @@ public abstract class Entity implements Positionable {
       g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f));
     }
   }
-  
-  /**
-   * Determines the path the entity should take
-   * @param goalCol Column of the goal location
-   * @param goalRow Row of the goal location
-   */
-  public void searchPath(int goalCol, int goalRow){
 
-    int startCol = (worldX + hitbox.x) / TILE_SIZE;
-    int startRow = (worldY + hitbox.y) / TILE_SIZE;
-
-    gp.pFinder.setNodes(startCol, startRow, goalCol, goalRow);
-
-    if(gp.pFinder.search()) {
-      // Next worldX and worldY
-      int nextX = gp.pFinder.pathList.get(0).col * TILE_SIZE;
-      int nextY = gp.pFinder.pathList.get(0).row * TILE_SIZE;
-
-      // Entity's hitbox positioning on the world map
-      int enLeftX = worldX + hitbox.x;
-      int enRightX = worldX + hitbox.x + hitbox.width;
-      int enTopY = worldY + hitbox.y;
-      int enBottomY = worldY + hitbox.y + hitbox.height;
-
-      if(enTopY > nextY && enLeftX >= nextX && enRightX < nextX + TILE_SIZE){
-        direction = directions.UP;
-      } else if(enBottomY < nextY && enLeftX >= nextX && enRightX < nextX + TILE_SIZE){
-        direction = directions.DOWN;
-      } else if(enTopY >= nextY && enBottomY < nextY + TILE_SIZE) {
-        if(enLeftX > nextX) {
-          direction = directions.LEFT;
-        } else if(enLeftX < nextX) {
-          direction = directions.RIGHT;
-        }
-      } else if(enTopY > nextY && enLeftX > nextX) {
-        direction = directions.UP;
-        checkCollision();
-        if(collision) {
-          direction = directions.LEFT;
-        }
-      } else if(enTopY > nextY && enLeftX < nextX) {
-        direction = directions.UP;
-        checkCollision();
-        if(collision) {
-          direction = directions.RIGHT;
-        }
-      } else if(enTopY < nextY && enLeftX > nextX) {
-        direction = directions.DOWN;
-        checkCollision();
-        if(collision) {
-          direction = directions.LEFT;
-        }
-      } else if(enTopY < nextY && enLeftX < nextX) {
-        direction = directions.DOWN;
-        checkCollision();
-        if(collision) {
-          direction = directions.RIGHT;
-        }
-      }
-      
-      int nextCol = gp.pFinder.pathList.get(0).col;
-      int nextRow = gp.pFinder.pathList.get(0).row;
-      if(nextCol == goalCol && nextRow == goalRow) {
-        onPath = false;
-      }
-    }
-  }
-  
   // Getter and setters for worldX and worldY below.
   public int getWorldX() { return worldX; }
   public void setWorldX(int worldX) { this.worldX = worldX; }
--- src/main/java/org/project/Monster.java
+++ src/main/java/org/project/Monster.java
@@ -59,11 +59,85 @@ public class Monster extends Entity {
     }
   }
 
+  /**
+   * method to set the behaviour of the monsters/slime.
+   * The searchPath helper function determines the path the entity should take to the player.
+   * The searchPath utilizes the pFinder (PathFinder class instance) which in turn utilizes
+   * the concept of A* path finding algorithm, a popular path finding algorithm in games.
+   */
   @Override
   public void setAction() {
     int goalCol = (gp.player.worldX + gp.player.hitbox.x) / TILE_SIZE;
     int goalRow = (gp.player.worldY + gp.player.hitbox.y) / TILE_SIZE;
     searchPath(goalCol, goalRow);
   }
+
+  /**
+   * Determines the path the monster should take to the player
+   * @param goalCol Column of the goal location
+   * @param goalRow Row of the goal location
+   */
+  public void searchPath(int goalCol, int goalRow){
+
+    int startCol = (worldX + hitbox.x) / TILE_SIZE;
+    int startRow = (worldY + hitbox.y) / TILE_SIZE;
+
+    gp.pFinder.setNodes(startCol, startRow, goalCol, goalRow);
+
+    if(gp.pFinder.search()) {
+      // Next worldX and worldY
+      int nextX = gp.pFinder.pathList.get(0).col * TILE_SIZE;
+      int nextY = gp.pFinder.pathList.get(0).row * TILE_SIZE;
+
+      // Entity's hitbox positioning on the world map
+      int enLeftX = worldX + hitbox.x;
+      int enRightX = worldX + hitbox.x + hitbox.width;
+      int enTopY = worldY + hitbox.y;
+      int enBottomY = worldY + hitbox.y + hitbox.height;
+
+      if(enTopY > nextY && enLeftX >= nextX && enRightX < nextX + TILE_SIZE){
+        direction = directions.UP;
+      } else if(enBottomY < nextY && enLeftX >= nextX && enRightX < nextX + TILE_SIZE){
+        direction = directions.DOWN;
+      } else if(enTopY >= nextY && enBottomY < nextY + TILE_SIZE) {
+        if(enLeftX > nextX) {
+          direction = directions.LEFT;
+        } else if(enLeftX < nextX) {
+          direction = directions.RIGHT;
+        }
+      } else if(enTopY > nextY && enLeftX > nextX) {
+        direction = directions.UP;
+        checkCollision();
+        if(collision) {
+          direction = directions.LEFT;
+        }
+      } else if(enTopY > nextY && enLeftX < nextX) {
+        direction = directions.UP;
+        checkCollision();
+        if(collision) {
+          direction = directions.RIGHT;
+        }
+      } else if(enTopY < nextY && enLeftX > nextX) {
+        direction = directions.DOWN;
+        checkCollision();
+        if(collision) {
+          direction = directions.LEFT;
+        }
+      } else if(enTopY < nextY && enLeftX < nextX) {
+        direction = directions.DOWN;
+        checkCollision();
+        if(collision) {
+          direction = directions.RIGHT;
+        }
+      }
+
+      int nextCol = gp.pFinder.pathList.get(0).col;
+      int nextRow = gp.pFinder.pathList.get(0).row;
+      if(nextCol == goalCol && nextRow == goalRow) {
+        onPath = false;
+      }
+    }
+  }
+
 }
 
